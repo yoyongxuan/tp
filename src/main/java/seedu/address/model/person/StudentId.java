8a0f4b5b1f88d3490b72789816fd221bf866cab7
JothamWong <45916998+JothamWong@users.noreply.github.com>
2025-10-03 00:50:49 +0800

    Add student id class

---
+++ src/main/java/seedu/address/model/person/StudentId.java
@@ -0,0 +1,61 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Person's student id in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidStudentId(String)}
+ */
+public class StudentId {
+
+    public static final String MESSAGE_CONSTRAINTS = "Student IDs should contain e as their first character and be "
+            + "followed by 7 numeric characters, and it should not be blank.";
+    public static final String VALIDATION_REGEX = "^e\\d{7}$";
+
+    public final String value;
+
+    /**
+     * Constructs a {@code StudentId}.
+     *
+     * @param id A valid student id.
+     */
+    public StudentId(String id) {
+        requireNonNull(id);
+        checkArgument(isValidStudentId(id), MESSAGE_CONSTRAINTS);
+        value = id;
+    }
+
+    /**
+     * Returns if a given string is a valid student id.
+     */
+    public static boolean isValidStudentId(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Email)) {
+            return false;
+        }
+
+        Email otherEmail = (Email) other;
+        return value.equals(otherEmail.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
