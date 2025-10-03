43fbba402dbfd784287fd7bd88a390c7cafb65c9
JothamWong <45916998+JothamWong@users.noreply.github.com>
2025-10-03 17:12:29 +0800

    Update indentation for JsonAdaptedPerson

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -20,7 +20,7 @@ import seedu.address.model.tag.Tag;
 public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         return new Person[]{
-                new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
+            new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                     new StudentId("A0000000A"),
                     new Address("Blk 30 Geylang Street 29, #06-40"),
                     getTagSet("friends")),
