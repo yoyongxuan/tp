8495df9da0853f58132379e77661195599c7cfe2
JothamWong <45916998+JothamWong@users.noreply.github.com>
2025-10-03 20:56:30 +0800

    Add TypicalStudentIds and update DeleteCommandTest to cover new path

---
+++ src/test/java/seedu/address/testutil/TypicalStudentIds.java
@@ -0,0 +1,11 @@
+package seedu.address.testutil;
+
+import seedu.address.model.person.StudentId;
+
+/**
+ * A utility class containing a list of {@code StudentId} objects to be used in tests.
+ */
+public class TypicalStudentIds {
+    public static final StudentId STUDENT_ID_A = new StudentId("A0000000A");
+    public static final StudentId STUDENT_ID_B = new StudentId("A0000000B");
+}
