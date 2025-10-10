52172a6dd31e41c39625a7d8fe68bad0903e23d2
yoyongxuan <66002397+yoyongxuan@users.noreply.github.com>
2025-10-10 17:29:20 +0800

    Add Identifier class and relevant tests

---
+++ src/main/java/seedu/address/commons/core/Identifier.java
@@ -0,0 +1,103 @@
+package seedu.address.commons.core;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+import java.util.List;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.StudentId;
+import seedu.address.model.person.exceptions.PersonNotFoundException;
+
+/**
+ * An Object that can be used to uniquely identify a person.
+ * Guarantees: immutable; is valid as declared in {@link #isValidIdentifier(String)}
+ */
+public class Identifier {
+
+    public static final String MESSAGE_CONSTRAINTS = "Identifier must be a valid Student ID or index.";
+
+    private final Index index;
+    private final StudentId studentId;
+    private final boolean isIndex;
+
+
+    /**
+     * Constructs a {@code Identifier} from either a student ID or an index.
+     *
+     * @param input A String representing either a valid student id or index.
+     */
+    public Identifier(String input) {
+        requireNonNull(input);
+        checkArgument(isValidIdentifier(input), MESSAGE_CONSTRAINTS);
+        isIndex = Index.isValidOneBasedIndex(input);
+
+        if (isIndex) {
+            index = Index.fromOneBased(Integer.parseInt(input));
+            studentId = null;
+        } else {
+            index = null;
+            studentId = new StudentId(input);
+        }
+    }
+
+    /**
+     * Returns if a given string is a valid student id or index.
+     */
+    public static boolean isValidIdentifier(String test) {
+        return Index.isValidOneBasedIndex(test) || StudentId.isValidStudentId(test);
+    }
+
+    /**
+     * Retrieves the {@code Person} object from the input model matching this {@code Identifier}.
+     * @param model The model containing the Address Book to retrieve Person from
+     * @return {@code Person} object in Address Book which matches this {@code Identifier}
+     * @throws PersonNotFoundException if no {@code Person} in Address Book matches this {@code Identifier}
+     */
+    public Person retrievePerson(Model model) throws PersonNotFoundException {
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+        Person out;
+
+        if (isIndex) {
+            if (index.getZeroBased() >= lastShownList.size()) {
+                throw new PersonNotFoundException();
+            }
+            out = lastShownList.get(index.getZeroBased());
+        } else {
+            out = lastShownList.stream()
+                    .filter(person -> person.getStudentId().equals(studentId))
+                    .findFirst()
+                    .orElseThrow(() -> new PersonNotFoundException());
+        }
+
+        return out;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Identifier)) {
+            return false;
+        }
+
+        Identifier otherIdentifier = (Identifier) other;
+
+        if (otherIdentifier.isIndex != this.isIndex) {
+            return false;
+        }
+
+        if (otherIdentifier.isIndex) {
+            return otherIdentifier.index.equals(this.index);
+        } else {
+            return otherIdentifier.studentId.equals(this.studentId);
+        }
+    }
+
+}
---
+++ src/test/java/seedu/address/commons/core/IdentifierTest.java
@@ -0,0 +1,128 @@
+package seedu.address.commons.core;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalPersons.BENSON;
+import static seedu.address.testutil.TypicalPersons.CARL;
+import static seedu.address.testutil.TypicalPersons.DANIEL;
+import static seedu.address.testutil.TypicalPersons.ELLE;
+import static seedu.address.testutil.TypicalPersons.FIONA;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+public class IdentifierTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Identifier(null));
+    }
+
+    @Test
+    public void constructor_invalidStudentId_throwsIllegalArgumentException() {
+        String invalidIdentifier = "";
+        assertThrows(IllegalArgumentException.class, () -> new Identifier(invalidIdentifier));
+    }
+
+    @Test
+    public void isValidIdentifier() {
+        // null identifier
+        assertThrows(NullPointerException.class, () -> Identifier.isValidIdentifier(null));
+
+        // blank identifier
+        assertFalse(Identifier.isValidIdentifier("")); // empty string
+        assertFalse(Identifier.isValidIdentifier(" ")); // spaces only
+
+        // invalid index
+        assertFalse(Identifier.isValidIdentifier("0"));
+        assertFalse(Identifier.isValidIdentifier("-1"));
+        assertFalse(Identifier.isValidIdentifier("one"));
+
+        // valid index
+        assertTrue(Identifier.isValidIdentifier("1"));
+        assertTrue(Identifier.isValidIdentifier("12345"));
+
+        // invalid studentId
+        assertFalse(Identifier.isValidIdentifier("a00000000z"));
+        assertFalse(Identifier.isValidIdentifier("b0000000z"));
+
+        // valid studentId
+        assertTrue(Identifier.isValidIdentifier("a0000000z"));
+        assertTrue(Identifier.isValidIdentifier("A0000000Z"));
+        assertTrue(Identifier.isValidIdentifier("a0123456z"));
+    }
+
+    @Test
+    public void retrievePerson() {
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+        // retrieve by index
+        Identifier indexIdentifier;
+
+        indexIdentifier = new Identifier("1");
+        assertEquals(ALICE, indexIdentifier.retrievePerson(model));
+
+        indexIdentifier = new Identifier("2");
+        assertEquals(BENSON, indexIdentifier.retrievePerson(model));
+
+        indexIdentifier = new Identifier("3");
+        assertEquals(CARL, indexIdentifier.retrievePerson(model));
+
+        // retrieve by StudentId
+        Identifier studentIdIdentifier;
+
+        studentIdIdentifier = new Identifier(DANIEL.getStudentId().toString());
+        assertEquals(DANIEL, studentIdIdentifier.retrievePerson(model));
+
+        studentIdIdentifier = new Identifier(ELLE.getStudentId().toString());
+        assertEquals(ELLE, studentIdIdentifier.retrievePerson(model));
+
+        studentIdIdentifier = new Identifier(FIONA.getStudentId().toString());
+        assertEquals(FIONA, studentIdIdentifier.retrievePerson(model));
+
+    }
+
+
+    @Test
+    public void equals() {
+        Identifier indexIdentifierA = new Identifier("1");
+        Identifier indexIdentifierB = new Identifier("2");
+
+        Identifier studentIdIdentifierA = new Identifier("a0000000z");
+        Identifier studentIdIdentifierB = new Identifier("a0123456b");
+
+        // same object -> returns true
+        assertTrue(indexIdentifierA.equals(indexIdentifierA));
+
+        // same values -> returns true
+        assertTrue(indexIdentifierA.equals(new Identifier("1")));
+        assertTrue(studentIdIdentifierA.equals(new Identifier("a0000000z")));
+
+        // same studentId in upper case -> return true
+        assertTrue(studentIdIdentifierA.equals(new Identifier("A0000000Z")));
+
+        // null -> returns false
+        assertFalse(indexIdentifierA.equals(null));
+
+        // different types -> returns false
+        assertFalse(indexIdentifierA.equals(new Object()));
+
+        // different index -> return false
+        assertFalse(indexIdentifierA.equals(indexIdentifierB));
+
+        // different studentId -> return false
+        assertFalse(studentIdIdentifierA.equals(studentIdIdentifierB));
+
+        // index identifer vs studentId identifier -> return false
+        assertFalse(indexIdentifierA.equals(studentIdIdentifierA));
+        assertFalse(studentIdIdentifierB.equals(indexIdentifierB));
+    }
+
+}
