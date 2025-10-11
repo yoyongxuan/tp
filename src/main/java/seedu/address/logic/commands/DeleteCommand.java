86461a4b31e0864ba1526fe154f9345fda73ea66
JothamWong <45916998+JothamWong@users.noreply.github.com>
2025-10-11 21:56:18 +0800

    Make Delete command use the Identifier class and update supporting tests

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -1,17 +1,14 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.Messages.MESSAGE_INVALID_STUDENT_ID_DISPLAYED_INDEX;
 
-import java.util.List;
-
-import seedu.address.commons.core.index.Index;
+import seedu.address.commons.core.Identifier;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.StudentId;
+import seedu.address.model.person.exceptions.PersonNotFoundException;
 
 /**
  * Deletes a person identified using it's displayed index from the address book.
@@ -28,55 +25,24 @@ public class DeleteCommand extends Command {
 
     public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
 
-    private final Index targetIndex;
-    private final StudentId targetStudentId;
-
+    private final Identifier identifier;
     /**
-     * Creates a DeleteCommand to delete the {@code Person} at the specified {@code index}.
-     * @param targetIndex The index to delete at.
+     * Creates a DeleteCommand to delete the {@code Person} at the specified {@code identifier}.
+     * @param identifier The identifier to delete at.
      */
-    public DeleteCommand(Index targetIndex) {
-        this.targetIndex = targetIndex;
-        this.targetStudentId = null;
-    }
-
-    /**
-     * Creates a DeleteCommand to delete the {@code Person} with the specified {@code targetStudentId}
-     * @param targetStudentId The student id of the contact to delete.
-     */
-    public DeleteCommand(StudentId targetStudentId) {
-        this.targetIndex = null;
-        this.targetStudentId = targetStudentId;
-    }
-
-    private CommandResult deleteWithIndex(Model model) throws CommandException {
-        List<Person> lastShownList = model.getFilteredPersonList();
-
-        if (targetIndex.getZeroBased() >= lastShownList.size()) {
-            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
-        }
-
-        Person personToDelete = lastShownList.get(targetIndex.getZeroBased());
-        model.deletePerson(personToDelete);
-        return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, Messages.format(personToDelete)));
-    }
-
-    private CommandResult deleteWithStudentId(Model model) throws CommandException {
-        Person personToDelete = model.getFilteredPersonList().stream()
-                .filter(person -> person.getStudentId().equals(targetStudentId))
-                .findFirst()
-                .orElseThrow(() -> new CommandException(MESSAGE_INVALID_STUDENT_ID_DISPLAYED_INDEX));
-        model.deletePerson(personToDelete);
-        return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, Messages.format(personToDelete)));
+    public DeleteCommand(Identifier identifier) {
+        this.identifier = identifier;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        if (targetStudentId != null) {
-            return deleteWithStudentId(model);
-        } else {
-            return deleteWithIndex(model);
+        try {
+            Person personToDelete = identifier.retrievePerson(model);
+            model.deletePerson(personToDelete);
+            return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, Messages.format(personToDelete)));
+        } catch (PersonNotFoundException pnfe) {
+            throw new CommandException(Messages.MESSAGE_PERSON_NOT_FOUND);
         }
     }
 
@@ -92,19 +58,14 @@ public class DeleteCommand extends Command {
         }
 
         DeleteCommand otherDeleteCommand = (DeleteCommand) other;
-        if (targetIndex != null) {
-            return targetIndex.equals(otherDeleteCommand.targetIndex);
-        } else if (targetStudentId != null) {
-            return targetStudentId.equals(otherDeleteCommand.targetStudentId);
-        } else {
-            return false;
-        }
+
+        return identifier.equals(otherDeleteCommand.identifier);
     }
 
     @Override
     public String toString() {
         return new ToStringBuilder(this)
-                .add("targetIndex", targetIndex)
+                .add("targetIndex", identifier)
                 .toString();
     }
 }
