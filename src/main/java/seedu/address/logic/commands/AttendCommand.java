fbea91ba107096c03e52d5558da975bbea95474a
jeremy <e1406441@u.nus.edu>
2025-10-13 11:25:08 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/commands/AttendCommand.java
+++ src/main/java/seedu/address/logic/commands/AttendCommand.java
@@ -6,7 +6,6 @@ import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import seedu.address.commons.core.Identifier;
 import seedu.address.commons.core.index.Index;
-import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
