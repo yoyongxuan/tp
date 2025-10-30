f358c0835ae308476277aefd2eabd7ab830682bd
yoyongxuan <66002397+yoyongxuan@users.noreply.github.com>
2025-10-30 16:57:25 +0800

    Remove unused import
    Signed-off-by: yoyongxuan <66002397+yoyongxuan@users.noreply.github.com>

--- src/test/java/seedu/address/logic/commands/AttendCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AttendCommandTest.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ATTENDANCE_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_STUDENT_ID_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIdentifiers.IDENTIFIER_FIRST_PERSON;
