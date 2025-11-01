11dcb9e2b4b2dc996922207d4f9e55858c5f2ab0
jeremy <e1406441@u.nus.edu>
2025-11-01 18:14:02 +0800

    Add additional tests for boundary testing

--- src/test/java/seedu/address/logic/parser/AttendCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AttendCommandParserTest.java
@@ -58,6 +58,15 @@ public class AttendCommandParserTest {
     }
 
     @Test
+    public void parse_maxInt_success() {
+        Identifier amyIdentifier = new Identifier(VALID_STUDENT_ID_AMY_STR);
+        AttendCommand attendCommand = new AttendCommand(amyIdentifier, Index.fromOneBased(Integer.MAX_VALUE));
+        assertParseSuccess(parser, AttendCommand.COMMAND_WORD + " " + VALID_STUDENT_ID_AMY_STR + " "
+                        + Integer.MAX_VALUE,
+                attendCommand);
+    }
+
+    @Test
     public void parse_invalidStudentId_failure() {
         assertParseFailure(parser, AttendCommand.COMMAND_WORD + " " + INVALID_STUDENT_ID_DESC + " 5",
                 MESSAGE_INVALID_FORMAT);
@@ -66,6 +75,18 @@ public class AttendCommandParserTest {
     @Test
     public void parse_invalidIndex_failure() {
         assertParseFailure(parser, AttendCommand.COMMAND_WORD + " " + VALID_STUDENT_ID_AMY_STR + " 0",
+                MESSAGE_WRONG_TUTORIAL);
+    }
+
+    @Test
+    public void parse_negativeIndex_failure() {
+        assertParseFailure(parser, AttendCommand.COMMAND_WORD + " " + VALID_STUDENT_ID_AMY_STR + " -1",
+                MESSAGE_WRONG_TUTORIAL);
+    }
+
+    @Test
+    public void parse_aboveMaxInt_failure() {
+        assertParseFailure(parser, AttendCommand.COMMAND_WORD + " 1" + " 2147483648",
                 MESSAGE_WRONG_TUTORIAL);
     }
 
