fe29953f59a75e22159d3c15b0fb5386ebaf2c46
dhdh <127590328+ndhhh@users.noreply.github.com>
2025-11-04 00:30:13 +0800

    Add more testing for tag validity

--- src/test/java/seedu/address/model/tag/TagTest.java
+++ src/test/java/seedu/address/model/tag/TagTest.java
@@ -25,6 +25,11 @@ public class TagTest {
         assertThrows(NullPointerException.class, () -> Tag.isValidTagName(null));
     }
 
+    /**
+     * Tests for valid tag names with accepted spacing and format.
+     * The tag name must be alphanumeric and can contain spaces.
+     * @author ndhhh
+     */
     @Test
     public void isValidTagName_validTagNames_returnsTrue() {
         // alphanumeric tag names
@@ -34,9 +39,15 @@ public class TagTest {
         assertTrue(Tag.isValidTagName("a1 b2 c3"));
 
         // long tag name
-        assertTrue(Tag.isValidTagName("beepboopbingbong with spaces 123456 r a   nd o m"));
+        assertTrue(Tag.isValidTagName("beepboopbingbong with spaces 12 3 4 5 6 r a n d o m"));
     }
 
+    /**
+     * Tests for invalid tag names with wrong spacing and format.
+     * The tag name cannot have leading or trailing spaces, multiple spaces between words,
+     * or non-alphanumeric characters.
+     * @author ndhhh
+     */
     @Test
     public void isValidTagName_invalidTagNames_returnsFalse() {
         // empty tag name
@@ -47,10 +58,23 @@ public class TagTest {
 
         // non-alphanumeric characters
         assertFalse(Tag.isValidTagName("friend!"));
+        assertFalse(Tag.isValidTagName("!"));
+        assertFalse(Tag.isValidTagName("?"));
+        assertFalse(Tag.isValidTagName("@"));
+        assertFalse(Tag.isValidTagName("!@#$%^&*()_+-={}[]|:;\"'<>,.?/"));
         assertFalse(Tag.isValidTagName("colleague@123"));
         assertFalse(Tag.isValidTagName("best#friend"));
         assertFalse(Tag.isValidTagName("hello_world"));
         assertFalse(Tag.isValidTagName("tag-with-dash"));
+
+        // Invalid spacing
+        assertFalse(Tag.isValidTagName(" friend "));
+        assertFalse(Tag.isValidTagName(" colleague"));
+        assertFalse(Tag.isValidTagName("bestfriend "));
+        assertFalse(Tag.isValidTagName("hello  world"));
+        assertFalse(Tag.isValidTagName(" tag  withdash"));
+        assertFalse(Tag.isValidTagName("fri  end "));
+        assertFalse(Tag.isValidTagName(" c o l l e a g u e @  123 "));
     }
 
     @Test
